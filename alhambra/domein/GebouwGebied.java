93e66700cd5e34464c80c0165e05b5046ad52619
Bram Van Sinay <91479859+BramVanSinay@users.noreply.github.com>
2025-03-27 16:14:24 +0100

    Update GebouwGebied.java

--- alhambra/domein/GebouwGebied.java
+++ alhambra/domein/GebouwGebied.java
@@ -1,1 +1,28 @@
+package domein;
 
+
+public class GebouwGebied {
+	private int[][] gebouwGebied;
+	
+	public GebouwGebied() {
+		gebouwGebied = new int[8][3];
+		initGebied();
+	}
+	
+	private int[][] initGebied() {
+		for (int rij = 0; rij < gebouwGebied.length; rij++) {
+            for (int kolom = 0; kolom < gebouwGebied[rij].length; kolom++) {
+                gebouwGebied[rij][kolom] = 8 - rij; 
+		
+            }
+	
+		}
+		return gebouwGebied;
+	}
+	
+	public int[][] getGebouwGebied() {
+		return gebouwGebied;
+	}
+	
+
+}
