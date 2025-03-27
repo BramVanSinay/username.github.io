2db5f65fa4a6770caee1a0689aae2c4804e16057
Bram Van Sinay <91479859+BramVanSinay@users.noreply.github.com>
2025-03-27 16:12:51 +0100

    Update DobbelsteenRepository.java

--- alhambra/domein/DobbelsteenRepository.java
+++ alhambra/domein/DobbelsteenRepository.java
@@ -1,1 +1,32 @@
+package domein;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DobbelsteenRepository {
+
+	private List<Dobbelsteen> dobbelstenen;
+
+	public DobbelsteenRepository() {
+		dobbelstenen = new ArrayList<>();
+//		dobbelstenen.add(new Dobbelsteen());
+//		dobbelstenen.add(new Dobbelsteen());
+//		dobbelstenen.add(new Dobbelsteen());
+//		dobbelstenen.add(new Dobbelsteen());
+//		dobbelstenen.add(new Dobbelsteen());
+//		dobbelstenen.add(new Dobbelsteen());
+//		dobbelstenen.add(new Dobbelsteen());
+//		dobbelstenen.add(new Dobbelsteen());
+
+	}
+
+	public int somDobbelstenen(int vorigeWorp) {
+		throw new UnsupportedOperationException();
+	}
+
+	public List<Dobbelsteen> getDobbelstenen() {
+		return dobbelstenen;
+	}
+
+}
 
