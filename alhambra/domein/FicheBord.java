bb635fe43c72cb8032e481c490f4a2062b56a98b
Bram Van Sinay <91479859+BramVanSinay@users.noreply.github.com>
2025-03-27 16:13:35 +0100

    Update FicheBord.java

--- alhambra/domein/FicheBord.java
+++ alhambra/domein/FicheBord.java
@@ -1,1 +1,15 @@
+package domein;
+
+public class FicheBord {
+	
+	private Fiche[] fiches = new Fiche[6];
+	
+	
+	private void vraagFichesOp() {
+		
+	}
+	public Fiche geefFiche(int index) {
+		return fiches[index];
+	}
+}
 
